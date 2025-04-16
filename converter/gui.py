b2d6cce361c5c279b735c2624b9328d067affad3
dimnissv <dimnissv@yandex.kz>
2025-04-16 17:49:14 +0500

    feat(gui): Add Settings tab with manual save/load/reset buttons
    Fixes #2

--- converter/gui.py
+++ converter/gui.py
@@ -27,7 +27,7 @@ class VideoConverterGUI:
             master: The root Tkinter window.
         """
         self.master = master
-        self.VERSION = '0.1.0'
+        self.VERSION = '0.1.5'
         self.TITLE = "JustConverter + AdBurner"
         self.AUTHOR = "dimnissv"
         master.title(f'{self.TITLE} ({self.AUTHOR}) {self.VERSION}')
